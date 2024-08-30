44967ba2915b603486919fa63912391c34b32fb5
brunozebendo <82032201+brunozebendo@users.noreply.github.com>
2024-08-30 09:16:54 -0300

    Update ReadMe.jsx

--- ReadMe.jsx
+++ ReadMe.jsx
@@ -5,4 +5,5 @@
  * para instlar o compilador que vai alertar sobre possíveis
  * erros digitar npx tsc ou npx tsc with-typescript.ts q é
  * para quando ainda não se configurou o ts
+ A explicação está em basics.ts
  */
